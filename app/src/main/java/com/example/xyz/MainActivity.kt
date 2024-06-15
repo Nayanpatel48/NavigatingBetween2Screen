77b70d41d508520dae3927d5b0357d4337f0251b
nayan <www.xil187@gmail.com>
2024-06-15 15:51:35 +0530

    Three screen interaction implimented

--- app/src/main/java/com/example/xyz/MainActivity.kt
+++ app/src/main/java/com/example/xyz/MainActivity.kt
@@ -7,9 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.example.xyz.ui.theme.XyzTheme
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -22,10 +22,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,20 +37,31 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
 
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(1) }
 
     Surface(modifier) {
-        if (shouldShowOnboarding) {
-                FirstScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            SecondScreen(onContinueClicked = { shouldShowOnboarding = true })
+        if (shouldShowOnboarding==1) {
+                FirstScreen(
+                    onButton1Clicked = { shouldShowOnboarding = 2 },
+                    onButton2Clicked = { shouldShowOnboarding = 3}
+                )
+        }
+        else if (shouldShowOnboarding==2) {
+            SecondScreen(onContinueClicked = { shouldShowOnboarding = 1 })
+        }
+        else if (shouldShowOnboarding==3) {
+            ThirdScreen(onContinueClicked = { shouldShowOnboarding = 1 })
+        }
+        else {
+            SecondScreen(onContinueClicked = { shouldShowOnboarding = 1 })
         }
     }
 }
 @Composable
 fun FirstScreen(
     modifier: Modifier = Modifier,
-    onContinueClicked: () -> Unit,
+    onButton1Clicked: () -> Unit,
+    onButton2Clicked: () -> Unit,
     ) {
     Column(
         modifier = modifier.fillMaxSize(),
@@ -62,16 +69,46 @@ fun FirstScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Welcome to the Basics Codelab!")
+        Row {
+            //button 1
+            Button(
+                modifier = Modifier
+                    .padding(vertical = 24.dp),
+                onClick = onButton1Clicked
+            ) {
+                Text("Screen2")
+            }
+            //button 2
+            Button(
+                modifier = Modifier
+                    .padding(vertical = 24.dp),
+                onClick = onButton2Clicked
+            ) {
+                Text("Screen3")
+            }
+        }
+    }
+}
+@Composable
+fun ThirdScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Screen-3")
         Button(
             modifier = Modifier
                 .padding(vertical = 24.dp),
             onClick = onContinueClicked
         ) {
-            Text("Continue")
+            Text("Back")
         }
     }
 }
-
 @Composable
 fun SecondScreen(modifier: Modifier = Modifier,
         onContinueClicked: () -> Unit
@@ -81,7 +118,7 @@ fun SecondScreen(modifier: Modifier = Modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to the Basics Codelab!")
+        Text("Screen-2")
         Button(
             modifier = Modifier
                 .padding(vertical = 24.dp),
@@ -95,7 +132,7 @@ fun SecondScreen(modifier: Modifier = Modifier,
 @Composable
 fun PreviewFirstScreen() {
     XyzTheme {
-        FirstScreen(modifier = Modifier.fillMaxSize(), { })
+        FirstScreen(modifier = Modifier.fillMaxSize(), { },{ })
     }
 }
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
@@ -103,5 +140,12 @@ fun PreviewFirstScreen() {
 fun PreviewSecondScreen() {
     XyzTheme {
         SecondScreen(modifier = Modifier.fillMaxSize(), { })
+    }
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun PreviewThirdScreen() {
+    XyzTheme {
+        ThirdScreen(modifier = Modifier.fillMaxSize(), { })
     }
 }
